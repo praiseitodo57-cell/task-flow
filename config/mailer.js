32418e0686ecbaf61dce342dbac7f20472a69aba
Itodo Praise <praiseitodo@gmail.com>
2026-04-29 05:45:26 +0100

    fix brevo v5 api

--- config/mailer.js
+++ config/mailer.js
@@ -1,7 +1,6 @@
-// config/mailer.js
-import SibApiV3Sdk from "@getbrevo/brevo";
+import * as Brevo from "@getbrevo/brevo";
 
-const client = new SibApiV3Sdk.TransactionalEmailsApi();
+const client = new Brevo.TransactionalEmailsApi();
 client.authentications["apiKey"].apiKey = process.env.BREVO_API_KEY;
 
 export { client as brevo };
