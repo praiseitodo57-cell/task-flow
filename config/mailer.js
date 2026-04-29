262f4b20ed138515cc98aa47b967d8955cf92cb2
Itodo Praise <praiseitodo@gmail.com>
2026-04-29 06:55:15 +0100

    adding of console to debug api key

--- config/mailer.js
+++ config/mailer.js
@@ -1,7 +1,9 @@
 import { BrevoClient } from "@getbrevo/brevo";
 
-const client = new BrevoClient({
-  apiKey: process.env.BREVO_API_KEY,
-});
+const apiKey = process.env.BREVO_API_KEY;
+console.log("[mailer] key length:", apiKey?.length);
+console.log("[mailer] key preview:", apiKey?.slice(0, 10));
+
+const client = new BrevoClient({ apiKey });
 
 export { client as brevo };
